038d3842d303c13710b406b17769a173ad9ea5f5
Toni <63047357+ToniGustavson@users.noreply.github.com>
2022-03-17 13:44:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pyomo.environ as pyo
+import pyomo.opt as opt
 
 model = pyo.ConcreteModel()
 
@@ -10,3 +11,9 @@ model.OBJ = pyo.Objective(expr = 2*model.x[1] + 3*model.x[2])
 model.Constraint1 = pyo.Constraint(expr = 3*model.x[1] + 4*model.x[2] >= 1)
 
 st.write(type(model))
+
+
+solver = "glpk"
+optimizer = opt.SolverFactory(solver)
+solver_info = optimizer.solve(pyM, tee=True)
+st.write(type(solver_info))
